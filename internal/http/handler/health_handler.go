ca6e3190ab9b30507988383adcdfefb1bc79d109
hamsakumar <satheeskumar.hamsakumar@pickme.lk>
2026-04-12 16:49:19 +0530

    health log added

--- internal/http/handler/health_handler.go
+++ internal/http/handler/health_handler.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/hamsacumar/travel_backend_api/internal/http/response"
@@ -10,5 +12,6 @@ import (
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
+	log.Printf(fmt.Sprintf(`health check working`))
 	_ = json.NewEncoder(w).Encode(response.HealthResponse{Status: "ok"})
 }
